8fe09f330a65915d1ca4b913cd89dcdb3d62478b
Kenny Shen <kenny@machinesung.com>
2018-10-07 09:30:10 +0000

    Basic exercises

--- lf/numbers.v
+++ lf/numbers.v
@@ -120,3 +120,186 @@ Example test_blt_nat2: (blt_nat 2 4) = true.
 Proof. simpl. reflexivity. Qed.
 Example test_blt_nat3: (blt_nat 4 2) = false.
 Proof. simpl. reflexivity. Qed.
+
+Theorem plus_0_n: forall n : nat, 0 + n = n.
+Proof.
+  intros n. simpl. reflexivity. Qed.
+
+(* Mostly equiv in Coq: Theorem, Example, Lemma, Fact, Remark *)
+
+Fact plus_1_l: forall n : nat, 1 + n = S n.
+Proof.
+  intros n. simpl. reflexivity. Qed.
+
+Theorem plus_id_example: forall n m : nat,
+  n = m ->
+  n + n = m + m.
+
+Proof.
+  (* move both quantifiers into context *)
+  intros n m.
+  (* move hypothesis into context *)
+  intros H.
+  (* rewrite goal using hypothesis *)
+  rewrite -> H.
+  reflexivity.
+  Qed.
+
+(* Exercise *)
+Theorem plus_id_exercise: forall n m o : nat,
+  n = m ->
+  m = o ->
+  n + m = m + o.
+
+Proof.
+  intros n m o.
+  intros H1.
+  rewrite -> H1.
+  intros H2.
+  rewrite <- H2.
+  reflexivity. Qed.
+
+(* Rewriting with a previously proven theorem *)
+Theorem mult_0_plus: forall n m : nat,
+  (0 + n) * m = n * m.
+
+Proof.
+  intros n m.
+  rewrite -> plus_0_n.
+  reflexivity. Qed.
+
+(* Exercise *)
+Theorem mult_S_1: forall n m : nat,
+  m = S n ->
+  m * (1 + n) = m * m.
+
+Proof.
+  intros n m.
+  intro H.
+  rewrite -> H.
+  reflexivity. Qed.
+
+(* Case analysis *)
+Theorem plus_1_neq_0: forall n : nat,
+  beq_nat (n + 1) 0 = false.
+
+Proof.
+  intros n.
+  destruct n as [| n' ]. (* cases: n = 0, n = S n' *)
+  (* bullets for marking proofs to each subgoal *)
+  - reflexivity.
+  - reflexivity.
+  Qed.
+
+Theorem negb_involutive: forall b : bool,
+  negb (negb b) = b.
+
+Proof.
+  intros b.
+  destruct b. (* no `as` clause, no binding required *)
+  - reflexivity.
+  - reflexivity.
+  Qed.
+
+(* Demonstrating nested subgoals *)
+Theorem andb_commutative : forall b c, andb b c = andb c b.
+
+Proof.
+  intros b c.
+  destruct b.
+  - destruct c.
+    + reflexivity.
+    + reflexivity.
+  - destruct c.
+    + reflexivity.
+    + reflexivity.
+  Qed.
+
+(* Demonstrating shorthand for `intros..destruct` *)
+Theorem plus_1_neq_0': forall n : nat,
+  beq_nat (n + 1) 0 = false.
+
+Proof.
+  intros [| n ].
+  - reflexivity.
+  - reflexivity.
+  Qed.
+
+Theorem andb_commutative' : forall b c, andb b c = andb c b.
+
+Proof.
+  intros [] [].
+  + reflexivity.
+  + reflexivity.
+  + reflexivity.
+  + reflexivity.
+  Qed.
+
+(* Exercise *)
+Theorem andb_true_elim2: forall b c : bool,
+  andb b c = true -> c = true.
+
+Proof.
+  intros b c.
+  destruct b.
+  - destruct c.
+    + reflexivity.
+    + discriminate.
+  - destruct c.
+    + reflexivity.
+    + discriminate.
+  Qed.
+
+(* Exercise *)
+Theorem zero_nbeq_plus_1: forall n : nat,
+  beq_nat 0 (n + 1) = false.
+
+Proof.
+  intros [| n ].
+  - reflexivity.
+  - reflexivity.
+  Qed.
+
+(* Exercise *)
+Theorem identity_fn_applied_twice:
+  forall (f : bool -> bool),
+  (forall (x : bool), f x = x) ->
+  forall (b : bool), f (f b) = b.
+
+Proof. (* It was more intuitive with the IDE *)
+  intros.
+  rewrite -> H.
+  rewrite -> H.
+  reflexivity.
+  Qed.
+
+(* Exercise *)
+Theorem negation_fn_applied_twice:
+  forall (f : bool -> bool),
+  (forall (x : bool), f x = negb x) ->
+  forall (b : bool), f (f b) = b.
+
+Proof.
+  intros.
+  rewrite H.
+  rewrite H.
+  rewrite -> negb_involutive.
+  reflexivity.
+  Qed.
+
+(* Exercise *)
+Theorem andb_eq_orb:
+  forall (b c : bool),
+  (andb b c = orb b c) ->
+  b = c.
+
+Proof.
+  intros.
+  destruct b.
+  - destruct c.
+    + simpl. reflexivity.
+    + simpl. discriminate.
+  - destruct c.
+    + simpl. discriminate.
+    + simpl. reflexivity.
+  Qed.
